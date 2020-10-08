f99a221ab76dee8548b590765695a167e474b01f
sqlps <pankaj.satyaketu@hotmail.com>
2020-10-08 15:55:51 -0400

    SSIS

---
+++ Azure/ADF-SSIS/Working With SSIS-IR.ps1
@@ -0,0 +1,22 @@
+﻿#Login-AzureRmAccount
+$DataFactoryName = 'ADF-V2'
+$AzureSsisIRName = 'pankajcsa-ir'
+$ResourceGroupName = 'ue2-pankajcsa-rg'
+
+#Get metadata about the Azure-SSIS IR
+Get-AzureRmDataFactoryV2IntegrationRuntime -DataFactoryName $DataFactoryName -Name $AzureSsisIRName -ResourceGroupName $ResourceGroupName
+
+#Get the status of the Azure-SSIS IR
+Get-AzureRmDataFactoryV2IntegrationRuntime -Status -DataFactoryName $DataFactoryName -Name $AzureSsisIRName -ResourceGroupName $ResourceGroupName 
+
+#Stop the IR
+Stop-AzureRmDataFactoryV2IntegrationRuntime -DataFactoryName $DataFactoryName -Name $AzureSsisIRName -ResourceGroupName $ResourceGroupName  -force
+
+#Get the status of the Azure-SSIS IR
+Get-AzureRmDataFactoryV2IntegrationRuntime -Status -DataFactoryName $DataFactoryName -Name $AzureSsisIRName -ResourceGroupName $ResourceGroupName
+
+#Start the IR
+Start-AzureRmDataFactoryV2IntegrationRuntime -DataFactoryName $DataFactoryName -Name $AzureSsisIRName -ResourceGroupName $ResourceGroupName -force
+
+#Get the status of the Azure-SSIS IR
+Get-AzureRmDataFactoryV2IntegrationRuntime -Status -DataFactoryName $DataFactoryName -Name $AzureSsisIRName -ResourceGroupName $ResourceGroupName 
